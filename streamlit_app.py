c8bd926ac6fd8641886c46b8159380ef9ba8f096
Fiona Wong <115694448+fionawongsf@users.noreply.github.com>
2022-10-13 15:11:37 +0100

    Update streamlit_app.py
    correct error in code

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text('🥑🍞 Advocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import panda
+import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
